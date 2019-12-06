4907fddd2b537e6ac98e78ab3ef5245a5e779cb1
gkm1987 <648750752@qq.com>
2019-12-06 09:25:11 +0800

    first commit

---
+++ src/test/java/it/cast/SpringTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
