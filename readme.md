27b7cfff801b9a5b80d1276a3da0cb0f0bce7548
unknown <648750752@qq.com>
2019-12-13 19:14:50 +0800

    zz

---
+++ readme.md
@@ -0,0 +1,3 @@
+2019/12/07
+
+> 在springboot 中使用  pagehelper 尝试分页
--- src/main/java/it/cast/mapper/DepartmentMapper.java
+++ src/main/java/it/cast/mapper/DepartmentMapper.java
@@ -20,4 +20,6 @@ public interface DepartmentMapper {
 
     @Update("update department set departmentName=#{department_name} where id=#{id}")
     public int updateDept(Department department);
+
+
 }
